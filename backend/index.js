e78e80592de9cf699151c4bf4bbcd93eb4698c1b
Vinayak Kumar <Vinayak0501>
2026-05-05 11:26:38 +0530

    fixed port issue on backend

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => {
     console.error("DB connection error:", err);
-    process.exit(1);
+    // Don't exit in production
   });
 
 // ===== Global Error Handler =====
